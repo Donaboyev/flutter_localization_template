f76ac3e6556a337c0f800b43ad5f1f964654ca8f
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2021-12-18 12:12:50 +0500

    cleaned unnecessary files and codes

--- lib/src/data/app_constants.dart
+++ lib/src/data/app_constants.dart
@@ -1,9 +1,5 @@
-const double systemTextScaleFactorOption = -1;
-const String hideChooseLangTab = 'hideChooseLangTab';
-const String uzCyrlScript = 'Cyrl';
 const String keyLocaleCode = 'LOCALE_CODE';
 const String keyLocaleScriptCode = 'LOCALE_SCRIPT_CODE';
-const String hiveBox = 'BOX';
 const String localeUz = 'uz';
 const String localeEn = 'en';
 const String localeRu = 'ru';
